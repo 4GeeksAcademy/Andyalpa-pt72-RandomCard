a2df041fd3fb9ab1580154eeeed71b5a2f59dde0
Andy Almeida <73726361+andyalpa@users.noreply.github.com>
2024-09-12 06:08:15 +0000

    Card Generetor Logic and Styles Created

--- src/app.js
+++ src/app.js
@@ -7,5 +7,67 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  generateRandomCard();
 };
+
+function generateRandomCard() {
+  const icons = [
+    // {
+    //   name: "hearts",
+    //   image: "/workspaces/Andyalpa-pt72-RandomCard/src/assets/img/hearts.png"
+    // },
+    // {
+    //   name: "clubs",
+    //   image: "/workspaces/Andyalpa-pt72-RandomCard/src/assets/img/clubs.png"
+    // },
+    // {
+    //   name: "spades",
+    //   image: "/workspaces/Andyalpa-pt72-RandomCard/src/assets/img/spades.png"
+    // },
+    // {
+    //   name: "diamond",
+    //   image: "/workspaces/Andyalpa-pt72-RandomCard/src/assets/img/diamond.png"
+    // }
+
+    "♥",
+    "♦",
+    "♠",
+    "♣"
+  ];
+  const numbers = ["A", "J", "Q", "K", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  const icon = Math.floor(Math.random() * icons.length);
+  const number = Math.floor(Math.random() * numbers.length);
+
+  document.querySelector(".randomCardValue").innerHTML = numbers[number];
+  const cardIcons = document.querySelectorAll("#topleft, #bottomright");
+
+  cardIcons.forEach(cardIcon => {
+    cardIcon.innerHTML = icons[icon];
+  });
+
+  console.log(icons[icon], numbers[number]);
+}
+
+document.querySelector(".btn").addEventListener("click", function() {
+  generateRandomCard();
+});
+
+function startTimer() {
+  let timeleft = 10;
+
+  const updateProgressBar = () => {
+    if (timeleft <= 0) {
+      generateRandomCard();
+      timeleft = 10;
+    }
+    document.getElementById("progressBar").value = 10 - timeleft;
+    timeleft -= 1;
+  };
+
+  updateProgressBar();
+
+  setInterval(updateProgressBar, 1000);
+}
+
+startTimer();
